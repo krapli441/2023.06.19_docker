fd5e4ad9dc9a4fff4a41dd53570881b738617a16
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-13 15:55:33 +0900

    실시간으로 바뀌는 것을 확인

--- ripple/App.tsx
+++ ripple/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>영훈아~</Text>
+      <Text>실시간으로 할 수 있다</Text>
       <StatusBar style="auto" />
     </View>
   );
